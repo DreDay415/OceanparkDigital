24f68c9d7ef74b3cd2958063b99ea8523e724530
DreDay415 <DreDay415@users.noreply.github.com>
2026-02-13 23:43:31 -0800

    chore: consolidate hosting fix to main

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  }),
   vite: {
     plugins: [tailwindcss()]
   },
